90b401a61c93caa1ca9a2ad87143a61cc7521188
zmeyer44 <zmmeyer44@gmail.com>
2023-10-13 23:22:07 -0400

    adding padding

--- app/(app)/_layout/index.tsx
+++ app/(app)/_layout/index.tsx
@@ -16,7 +16,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       {/* Sidebar */}
       <Sidebar />
       <div className="relative flex  flex-1 shrink-0 grow justify-center overflow-x-hidden">
-        <div className="flex-1 overflow-x-hidden px-5">{children}</div>
+        <div className="flex-1 overflow-x-hidden px-5 pb-5">{children}</div>
       </div>
       {/* Mobile Banner */}
       <MobileBanner />
