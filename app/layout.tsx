5db8be1c5b7316dcdf57766ee429f2984700017c
zmeyer44 <zmmeyer44@gmail.com>
2023-10-15 14:32:25 -0400

    splash screens

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,6 +58,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning className="">
+      <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-mobile-web-app-title" content="Flockstr" />
       <head>
         <link
           href="splashscreens/iphone5_splash.png"
