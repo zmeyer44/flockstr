10fb108f5abb6088beeb7c09b710ec7e07d552d4
zmeyer44 <zmmeyer44@gmail.com>
2023-10-20 15:32:17 -0400

    overflow fix

--- app/(app)/_layout/index.tsx
+++ app/(app)/_layout/index.tsx
@@ -20,7 +20,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       {/* Sidebar */}
       <Sidebar />
       <div className="relative flex  flex-1 shrink-0 grow justify-center overflow-x-hidden">
-        <div className="flex-1 pb-5">{children}</div>
+        <div className="flex-1 overflow-x-hidden pb-5">{children}</div>
       </div>
       {/* Mobile Banner */}
       <MobileBanner />
