af33ba0527f4c046152186d79dfc6e424300a5c9
zmeyer44 <zmmeyer44@gmail.com>
2023-10-23 15:03:16 -0400

    log it out

--- components/LocationSearch/index.tsx
+++ components/LocationSearch/index.tsx
@@ -104,7 +104,7 @@ function CommandSearch() {
               return (
                 <li
                   key={place_id}
-                  onClick={() => console.log()}
+                  onClick={() => console.log(place)}
                   className={cn(
                     "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 hover:bg-muted",
                   )}
