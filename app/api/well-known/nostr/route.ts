da065b35a25be9292980393b611b0c2671f85139
zmeyer44 <zmmeyer44@gmail.com>
2023-10-25 08:47:42 -0400

    testing new header

--- app/api/well-known/nostr/route.ts
+++ app/api/well-known/nostr/route.ts
@@ -6,7 +6,7 @@ function handler() {
       zach: "17717ad4d20e2a425cda0a2195624a0a4a73c4f6975f16b1593fc87fa46f2d58",
       _: "17717ad4d20e2a425cda0a2195624a0a4a73c4f6975f16b1593fc87fa46f2d58",
     },
-  });
+  }).headers.set("Content-Type", "application/json");
 }
 
 export { handler as GET, handler as POST };
