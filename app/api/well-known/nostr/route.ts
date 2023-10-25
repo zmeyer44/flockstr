181be8c9022c783a18b75dfa9d67ee328e97d7f2
zmeyer44 <zmmeyer44@gmail.com>
2023-10-25 08:49:52 -0400

    testing new changes

--- app/api/well-known/nostr/route.ts
+++ app/api/well-known/nostr/route.ts
@@ -1,12 +1,15 @@
 import { NextResponse } from "next/server";
 
 function handler() {
-  return NextResponse.json({
+  const response = NextResponse.json({
     names: {
       zach: "17717ad4d20e2a425cda0a2195624a0a4a73c4f6975f16b1593fc87fa46f2d58",
       _: "17717ad4d20e2a425cda0a2195624a0a4a73c4f6975f16b1593fc87fa46f2d58",
     },
-  }).headers.set("Content-Type", "application/json");
+  });
+  response.headers.set("Content-Type", "application/json");
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  return response;
 }
 
 export { handler as GET, handler as POST };
