4becfe1a946ef29a9043169d77cf2572e114156d
zmeyer44 <zmmeyer44@gmail.com>
2023-10-29 12:44:24 -0400

    auth protection

--- app/(app)/_layout/components/AddNoteButton.tsx
+++ app/(app)/_layout/components/AddNoteButton.tsx
@@ -1,21 +1,38 @@
+"use client";
+
 import { useModal } from "@/app/_providers/modal/provider";
 import { Button } from "@/components/ui/button";
 import { RiAddFill } from "react-icons/ri";
 import NewEventModal from "@/components/Modals/NewEvent";
+import LoginModal from "@/components/Modals/Login";
+import useCurrentUser from "@/lib/hooks/useCurrentUser";
 
 export default function AddNoteButton() {
   const modal = useModal();
+  const { currentUser } = useCurrentUser();
   return (
     <>
       <Button
-        onClick={() => modal?.show(<NewEventModal />)}
+        onClick={() => {
+          if (currentUser) {
+            return modal?.show(<NewEventModal />);
+          } else {
+            return modal?.show(<LoginModal />);
+          }
+        }}
         size={"icon"}
         className="xl:hidden"
       >
         <RiAddFill className="h-6 w-6" />
       </Button>
       <Button
-        onClick={() => modal?.show(<NewEventModal />)}
+        onClick={() => {
+          if (currentUser) {
+            return modal?.show(<NewEventModal />);
+          } else {
+            return modal?.show(<LoginModal />);
+          }
+        }}
         size={"lg"}
         className="hidden xl:flex"
       >
