87dbdef4e4496aecdeb9a40a7f512df37aa295a3
zmeyer44 <zmmeyer44@gmail.com>
2023-10-30 10:37:45 -0400

    adding auth guard to modal on mobile

--- app/(app)/explore/_components/NewEventButton.tsx
+++ app/(app)/explore/_components/NewEventButton.tsx
@@ -1,14 +1,24 @@
 "use client";
 import { useModal } from "@/app/_providers/modal/provider";
+import useCurrentUser from "@/lib/hooks/useCurrentUser";
 import NewEventModal from "@/components/Modals/NewEvent";
 import { RiAddFill } from "react-icons/ri";
+import LoginModal from "@/components/Modals/Login";
 import { Button } from "@/components/ui/button";
 
 export default function NewEventButton() {
   const modal = useModal();
+  const { currentUser } = useCurrentUser();
+
   return (
     <Button
-      onClick={() => modal?.show(<NewEventModal />)}
+      onClick={() => {
+        if (currentUser) {
+          return modal?.show(<NewEventModal />);
+        } else {
+          return modal?.show(<LoginModal />);
+        }
+      }}
       size={"icon"}
       className="h-[50px] w-[50px]"
     >
