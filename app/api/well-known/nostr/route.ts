9cc9f2b4a89b7ee608ec95ef42cf380be078f6f7
zmeyer44 <zmmeyer44@gmail.com>
2023-11-01 07:40:22 -0400

    add flockstr to nip-05

--- app/api/well-known/nostr/route.ts
+++ app/api/well-known/nostr/route.ts
@@ -4,7 +4,7 @@ function handler() {
   const response = NextResponse.json({
     names: {
       zach: "17717ad4d20e2a425cda0a2195624a0a4a73c4f6975f16b1593fc87fa46f2d58",
-      _: "17717ad4d20e2a425cda0a2195624a0a4a73c4f6975f16b1593fc87fa46f2d58",
+      _: "4fdf8b2c8bb34eff14f215fcdcf36dfd89120a473a5b0c993c1f48ae0252f147",
     },
   });
   response.headers.set("Content-Type", "application/json");
