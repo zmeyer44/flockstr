8c7577919c4bf1506dee306359421db9343508fa
Leonardo F Pendragon <124473641+anofibonacci@users.noreply.github.com>
2023-11-27 10:43:50 -0800

    revert to original state

--- containers/EventsTimeline/index.tsx
+++ containers/EventsTimeline/index.tsx
@@ -52,8 +52,12 @@ export function groupEventsByDay(events: NDKEvent[]) {
     if (!eventStartTime) continue;
     const startDate = fromUnix(parseInt(eventStartTime));
     const daysAway = daysOffset(startDate);
-    if (daysAway < -5) {
-       continue;
+    if (daysAway < 1) {
+      if (eventDays[`0`]) {
+        eventDays[`0`]!.push(event);
+      } else {
+        eventDays[`0`] = [event];
+      }
     } else if (eventDays[`${daysAway}`]) {
       eventDays[`${daysAway}`]!.push(event);
     } else {
